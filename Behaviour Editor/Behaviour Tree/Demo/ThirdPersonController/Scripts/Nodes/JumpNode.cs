f322f514e00880a7beb7adbfc3f5d7756a661706
Stellar-F0X <jms030907@gmail.com>
2025-05-25 00:51:35 +0900

    feat: BT Editor Demo 용도로 쓸 Starter Package 임포트

---
+++ Behaviour Editor/Behaviour Tree/Demo/ThirdPersonController/Scripts/Nodes/JumpNode.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+using BehaviourSystem.BT;
+
+[System.Serializable]
+public class JumpNode : ActionNode
+{
+    protected override void OnEnter() 
+    {
+    
+    }
+
+    protected override NodeBase.EBehaviourResult OnUpdate()
+    {
+        return NodeBase.EBehaviourResult.Failure;
+    }
+
+    protected override void OnExit()
+    {
+    
+    }
+}
